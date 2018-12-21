951d8a0f3e70e4528fd7ef7871f3ec048c9be425
Valentin Stavetski <vstavetski@gmail.com>
2018-12-21 21:26:42 +0300

    fixes

--- src/components/Table/Table.stories.jsx
+++ src/components/Table/Table.stories.jsx
@@ -1,37 +1,47 @@
-import React from 'react';
-import {storiesOf} from '@storybook/react';
-import Table, {TableRow, TableItem, TableAddRow} from './Table';
-import {AddButton, RemoveButton} from '../newregistry';
-import {LinkHash} from '../..';
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import Table, { TableRow, TableItem, TableAddRow } from "./Table";
+import { AddButton, RemoveButton } from "../newregistry";
+import { LinkHash } from "../..";
 
-storiesOf('Table', module)
-    .add('table', () => (
-        <Table>
-            <TableRow>
-                <TableItem>
-                    <LinkHash noCopy noPadding value="0x000F3754f028c704FDCe4e8ab9aAdba220f39618"/>
-                </TableItem>
-                <TableItem>1</TableItem>
-                <TableItem>1 %</TableItem>
-                <TableItem>
-                    <RemoveButton/>
-                </TableItem>
-            </TableRow>
-            <TableRow>
-                <TableItem>
-                    <LinkHash noCopy noPadding value="0x000F3754f028c704FDCe4e8ab9aAdba220f39618"/>
-                </TableItem>
-                <TableItem>2</TableItem>
-                <TableItem>50 %</TableItem>
-                <TableItem>
-                    <RemoveButton/>
-                </TableItem>
-            </TableRow>
-            <TableAddRow>
-                <input />
-                <input />
-                <span>100%</span>
-                <AddButton/>
-            </TableAddRow>
-        </Table>
-    ));
+storiesOf("Table", module)
+  .add("table", () => (
+    <Table>
+      <TableRow>
+        <TableItem>
+          <LinkHash noCopy noPadding value="0x000F3754f028c704FDCe4e8ab9aAdba220f39618"/>
+        </TableItem>
+        <TableItem>1</TableItem>
+        <TableItem>1 %</TableItem>
+        <TableItem>
+          <RemoveButton/>
+        </TableItem>
+      </TableRow>
+      <TableRow>
+        <TableItem>
+          <LinkHash noCopy noPadding value="0x000F3754f028c704FDCe4e8ab9aAdba220f39618"/>
+        </TableItem>
+        <TableItem>2</TableItem>
+        <TableItem>50 %</TableItem>
+        <TableItem>
+          <RemoveButton/>
+        </TableItem>
+      </TableRow>
+      <TableRow>
+        <TableItem>
+          <input/>
+        </TableItem>
+        <TableItem>
+
+          <input/>
+        </TableItem>
+        <TableItem>
+
+          <span>100%</span>
+        </TableItem>
+        <TableItem>
+          <AddButton/>
+        </TableItem>
+      </TableRow>
+    </Table>
+  ));
