0f8bffbc557bbb871d763c220ce5bc17804969e5
dimakorzhovnik <korzhovnik.dima@gmail.com>
2018-12-28 14:59:14 +0300

    inputs

--- src/components/newregistry/WideInput/WideInput.stories.jsx
+++ src/components/newregistry/WideInput/WideInput.stories.jsx
@@ -5,5 +5,4 @@ import {WideInput} from './WideInput';
 
 storiesOf('WideInput', module)
     .addDecorator(centered)
-    .add('WideInput', () => <WideInput placeholder="Name" defaultValue=""/>)
-    .add('WideInput validation', () => <WideInput placeholder="Name" defaultValue="" errorMessage='error' valid={false}/>);
+    .add('WideInput', () => <WideInput placeholder="Name" defaultValue=""/>);
