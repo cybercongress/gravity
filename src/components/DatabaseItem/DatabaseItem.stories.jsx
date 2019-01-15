6e0b6c2339ffca3c8d5513159c996cd2425fa3aa
dimakorzhovnik <korzhovnik.dima@gmail.com>
2019-01-15 15:45:35 +0300

    stories DatabaseItem

---
+++ src/components/DatabaseItem/DatabaseItem.stories.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import centered from '@storybook/addon-centered';
+import { addDecorator } from '@storybook/react';
+import { EditButton, UpdateButton, CancelButton, InfoButton, DeleteButton, FieldValue } from './index';
+
+
+storiesOf('commponents/DatabaseItem', module)
+    .addDecorator(centered)
+    .add('EditButton', () => <EditButton />)
+    .add('DeleteButton', () => <DeleteButton />)
+    .add('UpdateButton', () => <UpdateButton />)
+    .add('InfoButton', () => <InfoButton />)
+    .add('CancelButton', () => <CancelButton />)
+    .add('FieldValue', () => <FieldValue>fgfj</FieldValue>);
