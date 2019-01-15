4b5e524c4d77127a5b0a3733ff4b1ff4273dc1c7
dimakorzhovnik <korzhovnik.dima@gmail.com>
2019-01-15 16:01:29 +0300

    fix stories DbMenuPoints, DbMenu

--- src/components/DbMenu/DbMenu.stories.jsx
+++ src/components/DbMenu/DbMenu.stories.jsx
@@ -3,28 +3,32 @@ import { storiesOf } from '@storybook/react';
 import centered from '@storybook/addon-centered';
 import { addDecorator } from '@storybook/react';
 import {
+    DbMenu,
     MenuPopup,
     MenuPopupItem,
     MenuSeparator,
     MenuPopupAccountIcon,
     MenuPopupTransferIcon,
     MenuPopupDeleteIcon,
-    MenuPopupEditIcon,
+    MenuPopupPauseIcon,
+    MenuPopupResumeIcon,
 } from '../..';
-import { DbMenuPoints } from './DbMenuPoints';
 
 storiesOf('commponents/DbMenu', module)
     .addDecorator(centered)
-    .add('DbMenuPoints', () => (
-        <DbMenuPoints>
+    .add('DbMenu', () => (
+        <DbMenu>
             <MenuPopup>
                 <MenuPopupItem icon={ <MenuPopupAccountIcon /> }>Transfer Ownership</MenuPopupItem>
                 <MenuSeparator />
                 <MenuPopupItem icon={ <MenuPopupTransferIcon /> }>Fund Registry</MenuPopupItem>
+                <MenuPopupItem icon={ <MenuPopupTransferIcon /> }>Claim Fee</MenuPopupItem>
                 <MenuPopupItem icon={ <MenuPopupTransferIcon /> }>Claim Funds</MenuPopupItem>
                 <MenuSeparator />
+                <MenuPopupItem icon={ <MenuPopupPauseIcon /> }>Pause Regisrty</MenuPopupItem>
+                <MenuPopupItem icon={ <MenuPopupResumeIcon /> }>Resume Registry</MenuPopupItem>
+                <MenuSeparator />
                 <MenuPopupItem icon={ <MenuPopupDeleteIcon /> }> Delete Registry </MenuPopupItem>
-                <MenuPopupItem icon={ <MenuPopupEditIcon /> }> Delete Registry </MenuPopupItem>
             </MenuPopup>
-        </DbMenuPoints>
+        </DbMenu>
     ));
