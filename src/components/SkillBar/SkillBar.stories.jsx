f11e4bedb5bf9cce2f93a068ec001ac05c896735
dimakorzhovnik <korzhovnik.dima@gmail.com>
2019-01-16 18:58:40 +0300

    fix SkillBar stories

--- src/components/SkillBar/SkillBar.stories.jsx
+++ src/components/SkillBar/SkillBar.stories.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import centered from '@storybook/addon-centered';
-import { SkillBar } from '../..';
+import { SkillBar, PopupSkillBar, Text } from '../..';
 
+const PopupNotification = true;
 
-storiesOf('commponents/SkillsBar', module)
-
-    .add('SkillsBar', () => (
-        <div style={ { top: '50%', position: 'relative' } }>
-            <SkillBar value={ 10 } />
-        </div>
-    ));
+storiesOf('commponents/SkillsBar', module).add('SkillsBar', () => (
+    <div style={ { top: '50%', position: 'relative' } }>
+        <SkillBar value={ 10 }>
+            {PopupNotification && (
+                <PopupSkillBar>
+                    <Text color='white'>600000 of 1000000 left (60%) </Text>
+                </PopupSkillBar>
+            )}
+        </SkillBar>
+    </div>
+));
