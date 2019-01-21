4aedb735e22904f95a5cbda3ba6c2fd7dd18bbd6
dimakorzhovnik <korzhovnik.dima@gmail.com>
2019-01-21 13:54:47 +0300

    fix control

--- src/components/Control/Control.jsx
+++ src/components/Control/Control.jsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
+import { Text } from '../..';
 
 const styles = require('./Control.css');
 
 export const Control = ({ children, title }) => (
   <div className={styles.control}>
-    <label className={styles.controlLabel}>{title}</label>
+    <Text uppercase> {title} </Text>
     <div className={styles.controlComponent}>{children}</div>
   </div>
 );
