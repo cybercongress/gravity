91f45a8bd307c1932fc4cae9fef0633703a66a0c
dimakorzhovnik <korzhovnik.dima@gmail.com>
2019-01-30 18:09:18 +0300

    fix CybLink

--- src/components/CybLink/CybLink.js
+++ src/components/CybLink/CybLink.js
@@ -1,36 +1,63 @@
 import React, { Component } from 'react';
-import cx from 'classnames';
-// import { connect } from 'react-redux';
-// import { navigate } from '../redux/browser';
-const styles = require('./CybLink.css');
+// import cx from 'classnames';
+// // import { connect } from 'react-redux';
+// // import { navigate } from '../redux/browser';
+// const styles = require('./CybLink.css');
 
+class CybLink extends Component {
+    onLinkClick = (e) => {
+        e.preventDefault();
 
+        if (this.props.onNavigate) {
+            this.props.onNavigate(this.props.dura);
+        }
+    };
 
-export class CybLink extends Component {
-	onLinkClick = (e) => {
-	    e.preventDefault();
+    render() {
+        const {
+            children, dura, ...props
+        } = this.props;
 
-  		this.props.navigate(this.props.dura);
-
-	    if (this.props.onClick) { this.props.onClick(e); }
-  	}
-
-	render() {
-	    const {
-	        children, dura, defaultAccount, navigate, ...props
-	    } = this.props;
-
-	    return (
-			<a className={cx(styles.link,{[styles.id_bar__txq]: defaultAccount })}
-				href={ `cyb://${dura}` }
-				{ ...props }
-				onClick={ this.onLinkClick }
-			>
-					{this.props.children}
-			</a>
-	    );
-	}
+        return (
+            <a
+                href={ `cyb://${ dura }` }
+                { ...props }
+                onClick={ this.onLinkClick }
+            >
+                { this.props.children }
+            </a>
+        );
+    }
 }
+
+export default CybLink;
+
+
+// export class CybLink extends Component {
+// 	onLinkClick = (e) => {
+// 	    e.preventDefault();
+
+//   		this.props.navigate(this.props.dura);
+
+// 	    if (this.props.onClick) { this.props.onClick(e); }
+//   	}
+
+// 	render() {
+// 	    const {
+// 	        children, dura, defaultAccount, navigate, ...props
+// 	    } = this.props;
+
+// 	    return (
+// 			<a className={cx(styles.link,{[styles.id_bar__txq]: defaultAccount })}
+// 				href={ `cyb://${dura}` }
+// 				{ ...props }
+// 				onClick={ this.onLinkClick }
+// 			>
+// 					{this.props.children}
+// 			</a>
+// 	    );
+// 	}
+// }
 
 // export default connect(
 //     null,
