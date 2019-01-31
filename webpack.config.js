96c49a928cbbfe5ee07feeeb845c06e9895898d3
Valentin Stavetski <vstavetski@gmail.com>
2019-01-31 21:43:13 +0700

    fix less building

--- webpack.config.js
+++ webpack.config.js
@@ -48,10 +48,6 @@ module.exports = {
                     { loader: 'style-loader' },
                     {
                         loader: 'css-loader',
-                        options: {
-                            modules: true,
-                            localIdentName: '[name]_[local]',
-                        },
                     },
                 ],
             },
