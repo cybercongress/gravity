c555a31103963af4b8c6554fea27ed12bb6b8d0b
Valentin Stavetski <vstavetski@gmail.com>
2019-04-02 18:18:29 +0700

    refactor CardAccount for lottery page, export evergreen components

--- src/components/CardAccount/CardAccount.jsx
+++ src/components/CardAccount/CardAccount.jsx
@@ -1,102 +1,92 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
-    Pane, Text, Link, Pill, Card,
+    Pane, Text, Pill, Card,
 } from 'evergreen-ui';
-import { PaneBalance, Avatar } from '../..';
+import { PaneBalance } from '../..';
 
-export class CardAccount extends React.Component {
-    state = {
-        hover: false,
-    };
+export const CardAccount = ({
+    addressEth, balanceEth, addressCyberd, balanceCyberd,
+}) => {
+    const winner = !!addressCyberd;
+    const balanceCyberdGcyb = (balanceCyberd / 1000000000).toFixed(0);
 
-    handleHover() {
-        this.setState({ hover: !this.state.hover });
-    }
-
-    render() {
-        const { balanceGCYB } = this.props;
-
-        return (
-            <Card
-              boxShadow='0 0 1px #fff'
-                //   onMouseEnter={ () => this.handleHover() }
-                //   onMouseLeave={ () => this.handleHover() }
-              display='flex'
-              flexDirection='column'
-              alignItems='center'
-              backgroundColor='#000'
-              paddingX='3vw'
-              paddingY='10vh'
-              maxHeight={500}
+    return (
+        <Card
+          boxShadow='0 0 1px #fff'
+          display='flex'
+          flexDirection='column'
+          alignItems='center'
+          backgroundColor='#000'
+          paddingX='3vw'
+          paddingY='10vh'
+          maxHeight={ 500 }
+        >
+            <Text
+              display='inline-block'
+              marginBottom={ 155 }
+              color='#fff'
+              fontSize={ 24 }
+              fontWeight={ 600 }
+              className='fontSF'
             >
-                <Text
-                  display='inline-block'
-                  marginBottom={ 155 }
-                  color='#fff'
-                  fontSize={ 24 }
-                  fontWeight={ 600 }
-                  className='fontSF'
-                >
-                    {balanceGCYB != 0 ? 'Congratulations :-)' : 'You Loose :-('}
-                    {' '}
-                </Text>
-                <Pane width='100%' marginBottom={ 30 }>
-                    <Pane display='flex' flexDirection='row' marginBottom={ 45 }>
-                        <Pane>
-                            <Pill
-                              height={ 10 }
-                              width={ 10 }
-                              borderRadius='50%'
-                              color='yellow'
-                              paddingX={ 0 }
-                              isSolid
-                              marginRight={ 15 }
-                            />
-                        </Pane>
-                        <PaneBalance position='relative' display='flex' flexDirection='column'>
-                            <Text
-                              display='inline-block'
-                              marginBottom={ 11 }
-                              color='#fff'
-                              fontSize={ 30 }
-                            >
-                                148 ETH
-                            </Text>
-                            <Text display='inline-block' color='#fff'>
-                                0x92dF5e8886dA04fe4EB648d46e1Eeaaaa92256E5
-                            </Text>
-                        </PaneBalance>
+                {winner ? 'Congratulations :-)' : 'You Loose :-('}
+            </Text>
+            <Pane width='100%' marginBottom={30}>
+                <Pane display='flex' flexDirection='row' marginBottom={45}>
+                    <Pane>
+                        <Pill
+                          height={10}
+                          width={10}
+                          borderRadius='50%'
+                          color='yellow'
+                          paddingX={0}
+                          isSolid
+                          marginRight={15}
+                        />
                     </Pane>
-                    <Pane display='flex' flexDirection='row'>
-                        <Pane>
-                            <Pill
-                              height={ 10 }
-                              width={ 10 }
-                              borderRadius='50%'
-                              color='blue'
-                              paddingX={ 0 }
-                              isSolid
-                              marginRight={ 15 }
-                            />
-                        </Pane>
-                        <PaneBalance position='relative' display='flex' flexDirection='column'>
-                            <Text
-                              display='inline-block'
-                              marginBottom={ 11 }
-                              color='#fff'
-                              fontSize={ 30 }
-                            >
-                                {balanceGCYB}
-                                {' '}
-GCYB
-                            </Text>
-                            <Text display='inline-block' color='#fff'>
-                                cyber1f9yjqmxh6prsmgpcaqj8lmjnxg644n5074zznm
-                            </Text>
-                        </PaneBalance>
-                    </Pane>
+                    <PaneBalance position='relative' display='flex' flexDirection='column'>
+                        <Text
+                          display='inline-block'
+                          marginBottom={11}
+                          color='#fff'
+                          fontSize={30}
+                        >
+                            {balanceEth}
+                            &nbsp;ETH
+                        </Text>
+                        <Text display='inline-block' color='#fff'>
+                            {addressEth}
+                        </Text>
+                    </PaneBalance>
                 </Pane>
-            </Card>
-        );
-    }
-}
+                <Pane display='flex' flexDirection='row'>
+                    <Pane>
+                        <Pill
+                          height={10}
+                          width={10}
+                          borderRadius='50%'
+                          color='blue'
+                          paddingX={0}
+                          isSolid
+                          marginRight={15}
+                        />
+                    </Pane>
+                    <PaneBalance position='relative' display='flex' flexDirection='column'>
+                        <Text
+                          display='inline-block'
+                          marginBottom={11}
+                          color='#fff'
+                          fontSize={30}
+                        >
+                            {balanceCyberdGcyb}
+                            &nbsp;GCYB
+                        </Text>
+                        <Text display='inline-block' color='#fff'>
+                            {addressCyberd}
+                        </Text>
+                    </PaneBalance>
+                </Pane>
+            </Pane>
+        </Card>
+    );
+};
