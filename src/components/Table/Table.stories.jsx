574ffcabf28c7602871339c8de3109fb7b774c3a
dimakorzhovnik <korzhovnik.dima@gmail.com>
2019-05-24 18:32:32 +0300

    add stories Table stories

--- src/components/Table/Table.stories.jsx
+++ src/components/Table/Table.stories.jsx
@@ -1,75 +1,125 @@
-// import React from 'react';
-// import { storiesOf } from '@storybook/react';
-// import centered from '@storybook/addon-centered';
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import centered from '@storybook/addon-centered';
 // import Table, {
 //   TableRow,
 //   TableItem,
 //   TableAddRow,
 //   HomeTable,
 // } from './Table';
-// import { LinkHash, Checkbox,AddButton, RemoveButton  } from '../..';
+import { Table, IconButton, Pane } from 'evergreen-ui';
+import {
+    LinkHash, Checkbox, AddButton, RemoveButton,
+} from '../..';
 
-// storiesOf('Elements/Table', module)
-//   .addDecorator(centered)
-//   .add('table', () => (
-//     <Table>
-//       <TableRow>
-//         <TableItem>
-//           <LinkHash noCopy noPadding value="0x000F3754f028c704FDCe4e8ab9aAdba220f39618" />
-//         </TableItem>
-//         <TableItem>1</TableItem>
-//         <TableItem>1%</TableItem>
-//         <TableItem>
-//           <RemoveButton />
-//         </TableItem>
-//       </TableRow>
-//       <TableRow>
-//         <TableItem>
-//           <LinkHash noCopy noPadding value="0x000F3754f028c704FDCe4e8ab9aAdba220f39618" />
-//         </TableItem>
-//         <TableItem>2</TableItem>
-//         <TableItem>50%</TableItem>
-//         <TableItem>
-//           <RemoveButton />
-//         </TableItem>
-//       </TableRow>
-//     </Table>
-//   ))
+const addRegistryItem = [
+    {
+        name: 'yyg',
+        hash: 'QmS2F1UgmYHAekcvezFLCrBibEBjJezSDqwq8fuwF5Qqvi',
+        protocol: 'ipfs',
+    },
+    {
+        name: 'dfgd1',
+        hash: 'QmS2F1UgmYHAekcvezFLCrBibEBjJezSDqwq8fuwF5Qqvi',
+        protocol: 'ipfs',
+    },
+    {
+        name: 'dfgd2',
+        hash: 'QmS2F1UgmYHAekcvezFLCrBibEBjJezSDqwq8fuwF5Qqvi',
+        protocol: 'ipfs',
+    },
+    {
+        name: 'dfgd3',
+        hash: 'QmS2F1UgmYHAekcvezFLCrBibEBjJezSDqwq8fuwF5Qqvi',
+        protocol: 'ipfs',
+    },
+];
 
-//   .add('table_Input', () => (
-//     <Table>
-//       <TableAddRow>
-//         <input />
-//         <input />
-//         <span>100%</span>
-//         <AddButton />
-//       </TableAddRow>
-//     </Table>
-//   ))
+const rowsBlack = addRegistryItem.map((item, index) => (
+    <Table.Row borderBottom='none' paddingLeft={ 20 } height={ 50 } isSelectable key={ index }>
+        <Table.TextCell>
+            <span style={ { color: '#fff', fontSize: 17 } }>
+.
+                {item.name}
+            </span>
+        </Table.TextCell>
+        <Table.TextCell flexShrink={ 0 } flexGrow={ 2 }>
+            <span style={ { color: '#fff', fontSize: 17 } }>{item.hash}</span>
+        </Table.TextCell>
+        {/* <Table.TextCell>{item.protocol}</Table.TextCell> */}
+        <Table.Cell width={ 60 } flex='none'>
+            {/* <Button sizeSm color='blue' onClick={ () => this.deleteRegistryItem(item.name) }> */}
 
-//   .add('table_3', () => (
-//     <HomeTable>
-//       <tbody>
-//         <tr>
-//           <td>Address</td>
-//           <td>address</td>
-//           <td>
-//             <Checkbox>unique</Checkbox>
-//           </td>
-//           <td>
-//             <RemoveButton />
-//           </td>
-//         </tr>
-//         <tr>
-//           <td>Address</td>
-//           <td>address</td>
-//           <td>
-//             <Checkbox>unique</Checkbox>
-//           </td>
-//           <td>
-//             <RemoveButton />
-//           </td>
-//         </tr>
-//       </tbody>
-//     </HomeTable>
-//   ));
+            <Pane paddingY={ 5 } paddingX={ 5 } width='100%'>
+                <IconButton
+                  iconSize={ 12 }
+                  className='icon-btn color-white-svg'
+                  appearance='minimal'
+                  icon='more'
+                />
+            </Pane>
+
+            {/* </Button> */}
+        </Table.Cell>
+    </Table.Row>
+));
+
+const rows = addRegistryItem.map((item, index) => (
+    <Table.Row paddingLeft={ 20 } height={ 50 } isSelectable key={ index }>
+        <Table.TextCell>
+.
+            {item.name}
+        </Table.TextCell>
+        <Table.TextCell flexShrink={ 0 } flexGrow={ 2 }>
+            {item.hash}
+        </Table.TextCell>
+        {/* <Table.TextCell>{item.protocol}</Table.TextCell> */}
+        <Table.Cell width={ 60 } flex='none'>
+            {/* <Button sizeSm color='blue' onClick={ () => this.deleteRegistryItem(item.name) }> */}
+
+            <Pane paddingY={ 5 } paddingX={ 5 } width='100%'>
+                <IconButton iconSize={ 12 } className='icon-btn' appearance='minimal' icon='more' />
+            </Pane>
+
+            {/* </Button> */}
+        </Table.Cell>
+    </Table.Row>
+));
+
+storiesOf('Elements/Table', module)
+    .add('Table Black', () => (
+        <Pane paddingX='5em' paddingY='5em' width='100%' height='100%' backgroundColor='#000'>
+            <Table>
+                <Table.Head
+                  style={ { backgroundColor: '#000', borderBottom: '1px solid #ffffff80' } }
+                  paddingLeft={ 20 }
+                >
+                    <Table.TextHeaderCell>
+                        <span style={ { color: '#fff', fontSize: 17 } }>Name</span>
+                    </Table.TextHeaderCell>
+                    <Table.TextHeaderCell flexShrink={ 0 } flexGrow={ 2 }>
+                        <span style={ { color: '#fff', fontSize: 17 } }>IPFS Hash</span>
+                    </Table.TextHeaderCell>
+                    <Table.TextHeaderCell width={ 60 } flex='none' />
+                </Table.Head>
+                <Table.Body style={ { backgroundColor: '#000', overflowY: 'hidden' } }>
+                    {rowsBlack}
+                </Table.Body>
+            </Table>
+        </Pane>
+    ))
+
+    .add('Table', () => (
+        <Pane paddingX='5em' paddingY='5em' width='100%' height='100%' backgroundColor='#000'>
+            <Table>
+                <Table.Head paddingLeft={ 20 }>
+                    <Table.TextHeaderCell>Name</Table.TextHeaderCell>
+                    <Table.TextHeaderCell flexShrink={ 0 } flexGrow={ 2 }>
+                        IPFS Hash
+                    </Table.TextHeaderCell>
+                    <Table.TextHeaderCell width={ 60 } flex='none' />
+                </Table.Head>
+                <Table.Body style={ { backgroundColor: '#fff', overflowY: 'hidden' } }>{rows}</Table.Body>
+            </Table>
+        </Pane>
+    ));
