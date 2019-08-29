6d6dfd34ecefe7d37c1b2f5e495238793a9ff553
dimakorzhovnik <korzhovnik.dima@gmail.com>
2019-08-29 03:34:18 +0300

    cyber.auction

--- src/components/Text/Text.stories.jsx
+++ src/components/Text/Text.stories.jsx
@@ -1,50 +1,12 @@
-// import React from 'react';
-// import { storiesOf } from '@storybook/react';
-// import centered from '@storybook/addon-centered';
-// import { addDecorator } from '@storybook/react';
-// import { Text } from '../..';
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { Paragraph } from './Text';
 
-// storiesOf('Atoms|Text', module)
-//     .addDecorator(centered)
-//     .add('TextSize', () => (
-//         <div>
-//             <Text size='sm'>TextSM</Text>
-//             <Text>Text Normal</Text>
-//             <Text size='lg'>TextLG</Text>
-//             <Text size='xlg'>TextXLG</Text>
-//             <Text size='xxlg'>TextXXLG</Text>
-//         </div>
-//     ))
-//     .add('TextColor', () => (
-//         <div>
-//             <Text size='sm'>TextSM</Text>
-//             <Text>Text Normal</Text>
-
-//             <div style={ { backgroundColor: 'black' } }>
-//                 <Text color='white' size='lg'>
-//                     TextLG
-//                 </Text>
-//             </div>
-//             <Text color='blue' size='xlg'>
-//                 TextXLG
-//             </Text>
-//             <Text  color='black' size='xxlg'>
-//                 TextXXLG
-//             </Text>
-//         </div>
-//     ))
-//     .add('Text', () => (
-//       <div>
-//           <Text size='sm'>TextSM</Text>
-//           <Text>Text Normal </Text>
-//               <Text lineheight size='lg'>
-//                   TextLG lineheight
-//               </Text>
-//           <Text bold size='xlg'>
-//               TextXLG bold
-//           </Text>
-//           <Text uppercase size='xxlg'>
-//               TextXXLG uppercase
-//           </Text>
-//       </div>
-//   ));
+storiesOf('Atoms|Typography', module)
+    .add('Typography', () => (
+        <div>
+            {/* <Caption>Caption</Caption>
+            <Chapter>Chapter</Chapter> */}
+            <Paragraph>Paragraph</Paragraph>
+        </div>
+    ));
