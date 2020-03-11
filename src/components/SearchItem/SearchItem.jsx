0e3d349a40a5934532889f85aec890a7cf653550
dimakorzhovnik <korzhovnik.dima@gmail.com>
2020-03-11 20:27:43 +0300

    update  SearchItem

--- src/components/SearchItem/SearchItem.jsx
+++ src/components/SearchItem/SearchItem.jsx
@@ -1,178 +1,196 @@
-import * as React from 'react';
-import {
-    Pane, Pill, Text, Spinner, Tooltip, Link,
-} from 'evergreen-ui';
-import styles from './SearchItem.less';
+import * as React from "react";
+import { Pane, Pill, Text, Spinner, Tooltip, Link } from "evergreen-ui";
+import styles from "./SearchItem.less";
 // import cx from 'classnames';
 
-export class SearchItem extends React.Component {
-    state = {
-        hover: false,
-    };
+const LinkWithTooltip = ({ status, hash, content }) => {
+  let colorPill = "neutral";
 
-    handleHover() {
-        this.setState({ hover: !this.state.hover });
-    }
+  switch (status) {
+    case "failed":
+      colorPill = "red";
+      break;
+    case "loading":
+      colorPill = "yellow";
+      break;
+    case "success":
+      colorPill = "green";
+      break;
+    default:
+      colorPill = "neutral";
+      break;
+  }
 
-    render() {
-        const { status, rank, grade, onClick, children } = this.props;
-
-        let rankGradeColor = 'neutral';
-        switch (grade.value) {
-          case 1:
-            rankGradeColor = 'red';
-            break;
-          case 2:
-            rankGradeColor = 'orange';
-            break;
-          case 3:
-            rankGradeColor = 'yellow';
-            break;
-          case 4:
-            rankGradeColor = 'green';
-            break;
-          case 5:
-            rankGradeColor = 'teal';
-            break;
-          case 6:
-            rankGradeColor = 'blue';
-            break;
-          case 7:
-            rankGradeColor = 'purple';
-            break;
-          default:
-            rankGradeColor = 'neutral';
-            break;
-        }
-
-        const linkWithTooltip = (
-          <Pane display='flex' alignItems='center'>
-            <Tooltip
-              appearance='card'
-              content={ (
-                <Pane paddingX={ 18 } paddingY={ 18 }>
-                  <Pane display='flex' alignItems='center' marginBottom={ 10 }>
-                    <Text>
-                      {status === 'loading' && 'Receiving answer from IPFS'}
-                      {status === 'failed' && 'Can’t receive answer from IPFS'}
-                      {status === 'success' && 'Answer received from IPFS'}
-                    </Text>
-                  </Pane>
-{/*                  <Pane>
-                    <Text>
-                      More on{' '}
-                      <Link textDecoration='none' color='green' cursor='pointer'>
-                        IPFS
-                      </Link>
-                    </Text>
-                  </Pane>*/}
-                </Pane>
-              ) }
-            >
-              <Text
-                color={ this.state.hover ? '#979797' : '#000'}
-                size={ 400 }
-              >
-                {children}
-              </Text>
-            </Tooltip>
-          </Pane>
-        );
-
-        const gradeTooltipContent = (
-          <Pane paddingX={ 18 } paddingY={ 18 }>
-            <Pane marginBottom={ 12 }>
-              <Text>Answer rank is {rank}</Text>
-            </Pane>
-            <Pane display='flex' marginBottom={ 12 }>
-              <Text>
-                Answers between	&nbsp;{grade.from}&nbsp; and &nbsp;{grade.to}&nbsp; recieve grade
-                <Pill
-                  paddingX={ 8 }
-                  paddingY={ 5 }
-                  width={ 23 }
-                  marginLeft={ 5 }
-                  display='inline-flex'
-                  alignItems='center'
-                  style={ { color: '#fff' } }
-                  color={ rankGradeColor }
-                  isSolid
-                >
-                  {grade.value}
-                </Pill>
-              </Text>
-            </Pane>
-{/*            <Pane>
-              <Text>
-                More on{' '}
-                <Link textDecoration='none' color='green' cursor='pointer'>
-                  cyber•Rating
-                </Link>
-              </Text>
-            </Pane>*/}
-          </Pane>
-        );
-
-        const rankGradeWithTooltip = (
+  return (
+    <Pane display="flex" flex={1} alignItems="center">
+      <Pane display="flex" flex={1} flexDirection="column">
+        <Pane
+          display="flex"
+          alignItems="center"
+          marginBottom={status === "success" ? 10 : "none"}
+        >
           <Tooltip
-            appearance='card'
-            content={ gradeTooltipContent }
+            appearance="card"
+            content={
+              <Pane paddingX={18} paddingY={18}>
+                <Pane display="flex" alignItems="center" marginBottom={10}>
+                  <Text>
+                    {status === "loading" && "Receiving answer from IPFS"}
+                    {status === "failed" && "Can’t receive answer from IPFS"}
+                    {status === "success" && "Answer received from IPFS"}
+                  </Text>
+                </Pane>
+              </Pane>
+            }
           >
             <Pill
-              paddingX={ 8 }
-              paddingY={ 5 }
-              display='flex'
-              alignItems='center'
-              style={ { color: '#fff' } }
-              color={ rankGradeColor }
+              paddingX={4}
+              paddingY={4}
+              width={4}
+              height={4}
+              display="flex"
+              alignItems="center"
+              style={{ color: "#fff" }}
+              color={colorPill}
               isSolid
-            >
-              {grade.value}
-            </Pill>
+              marginRight={8}
+            />
           </Tooltip>
-        );
 
-        const item = (
-          <Pane
-            boxShadow={ '0px 0px 0.4px 0px #dedede' }
-            onMouseEnter={ () => this.handleHover() }
-            onMouseLeave={ () => this.handleHover() }
-            paddingLeft={ 20 }
-            paddingRight={ 10 }
-            paddingY={ 12 }
-            display='flex'
-            justifyContent='space-between'
-            key={ this.props.hash }
-            backgroundColor={'#fff'}
-            borderRadius={ 5 }
-            marginBottom={ 10 }
-            onClick={ onClick }
+          <Text color="#000" width="fit-content" size={500}>
+            {hash}
+          </Text>
+        </Pane>
+        {status === "success" && (
+          <Text flex={1} color="#000" size={400}>
+            {content}
+          </Text>
+        )}
+      </Pane>
+    </Pane>
+  );
+};
+
+export const SearchItem = ({
+  status,
+  rank,
+  grade,
+  onClick,
+  children,
+  hash
+}) => {
+  let rankGradeColor = "neutral";
+
+  switch (grade.value) {
+    case 1:
+      rankGradeColor = "red";
+      break;
+    case 2:
+      rankGradeColor = "orange";
+      break;
+    case 3:
+      rankGradeColor = "yellow";
+      break;
+    case 4:
+      rankGradeColor = "green";
+      break;
+    case 5:
+      rankGradeColor = "teal";
+      break;
+    case 6:
+      rankGradeColor = "blue";
+      break;
+    case 7:
+      rankGradeColor = "purple";
+      break;
+    default:
+      rankGradeColor = "neutral";
+      break;
+  }
+
+  const gradeTooltipContent = (
+    <Pane paddingX={15} paddingY={15}>
+      <Pane marginBottom={12}>
+        <Text>Answer rank is {rank}</Text>
+      </Pane>
+      <Pane display="flex" marginBottom={12}>
+        <Text>
+          Answers between &nbsp;
+          {grade.from}
+          &nbsp; and &nbsp;
+          {grade.to}
+          &nbsp; recieve grade
+          <Pill
+            paddingX={8}
+            paddingY={5}
+            width={23}
+            marginLeft={5}
+            display="inline-flex"
+            alignItems="center"
+            style={{ color: "#fff" }}
+            color={rankGradeColor}
+            isSolid
           >
-            {linkWithTooltip}
+            {grade.value}
+          </Pill>
+        </Text>
+      </Pane>
+    </Pane>
+  );
 
-            {status === 'success' && rankGradeWithTooltip }
+  const rankGradeWithTooltip = (
+    <Tooltip appearance="card" content={gradeTooltipContent}>
+      <Pill
+        paddingX={8}
+        paddingY={5}
+        display="flex"
+        alignItems="center"
+        style={{ color: "#fff" }}
+        color={rankGradeColor}
+        isSolid
+      >
+        {grade.value}
+      </Pill>
+    </Tooltip>
+  );
 
-            {status === 'loading' && (
-              <Spinner size={ 16 } />
-            )}
-          </Pane>
-        );
+  const item = (
+    <Pane
+      boxShadow="0px 0px 0.4px 0px #dedede"
+      paddingLeft={20}
+      paddingRight={10}
+      paddingY={12}
+      display="flex"
+      justifyContent="space-between"
+      key={hash}
+      backgroundColor="#fff"
+      borderRadius={5}
+      marginBottom={10}
+      onClick={onClick}
+      className={styles.containerSearchItem}
+      maxHeight="250px"
+    >
+      <LinkWithTooltip status={status} hash={hash} content={children} />
+      {status === "loading" && <Spinner marginX={10} size={16} />}
+      {rankGradeWithTooltip}
+    </Pane>
+  );
 
-        return (
-          <span>
-            {status === 'success' ? (
-              <a
-                style={{
-                  textDecoration: "none"
-                }}
-                href={`cyb://${children}`}
-              >
-                {item}
-              </a>
-            ) :
-              item
-            }
-          </span>
-        );
-    }
-}
+  return (
+    <span>
+      {status === "success" ? (
+        <a
+          style={{
+            textDecoration: "none"
+          }}
+          href={`https://ipfs.io/ipfs/${hash}`}
+          target="_blank"
+        >
+          {item}
+        </a>
+      ) : (
+        item
+      )}
+    </span>
+  );
+};
--- src/components/SearchItem/SearchItem.stories.jsx
+++ src/components/SearchItem/SearchItem.stories.jsx
@@ -1,27 +1,62 @@
-// import React from 'react';
-// import { storiesOf } from '@storybook/react';
-// import centered from '@storybook/addon-centered';
-// import { SearchItem, LinkContainer } from '../..';
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { Pane } from "evergreen-ui";
 
+import { SearchItem } from "./SearchItem";
 
-// storiesOf('Atoms|SearchResult', module)
-//     .add('SearchItem', () => (
-//         <div style={ { top: '50%', position: 'relative' } }>
-//             <SearchItem hash='sdfs21312' rank='2'>
-//                 content
-//             </SearchItem>
-//         </div>
-//     ))
-//     .add('SearchResult', () => (
-//         <LinkContainer column>
-//             <SearchItem hash='sdfs21312' rank='2'>
-//                 content
-//             </SearchItem>
-//             <SearchItem hash='sdfs21312' rank='2'>
-//                 content
-//             </SearchItem>
-//             <SearchItem hash='sdfs21312' rank='2'>
-//                 content
-//             </SearchItem>
-//         </LinkContainer>
-//     ));
+const links = {
+  QmeaYGKqdJHCstEPsS4iepvT5y6JZyuAKu87kFwz1yQEyx: {
+    rank: "0.000576",
+    grade: { from: 0.000001, to: 0.001, value: 3 },
+    status: "success",
+    content: "data:text/plain;base64,SGV5IQoKSSBoYXZlIG5vIGlkZWEgd2hhdCB0aGlzIG1lYW5zLiBCdXQgSSBndWVzcyB5b3UgY2FuIGtub3cgZm9yIHN1cmUuIEN5YmVybGluayBpdC4KCldoYXQgaXMgY3liZXJsaW5rPwoKWW91IGhhdmUgcHJvYmFibHkgdXNlZCBsaW5rcyB3aGljaCBsb29rIGxpa2UgdGhpczogaHR0cHM6Ly9nb29nbGUuY29tLiBFdmVudHVhbGx5IHRoZSByZXF1ZXN0IHRvIHRoaXMgbGluayByZXNvbHZlcyB0byBhIHBhcnRpY3VsYXIgbWFjaGluZSB3aGljaCB3aWxsIHNob3cgeW91ciBvbGQgZ29kLiBZb3UgY2FudCBrbm93IGZvciBzdXJlIHdoYXQgeW91IGdldC4gQ3liZXJsaW5rcyBhcmUgZGlmZmVyZW50LiBDeWJlcmxpbmsgaXMgYSBsaW5rIGJldHdlZW4gdHdvIHBpZWNlcyBvZiBjb250ZW50IHRoZXJlIGVhY2ggY29udGVudCBpcyBhZGRyZXNzZWQgYnkgaXRzIGhhc2guIENoYW5naW5nIHRoZSB3YXkgd2UgbGluayB0aGluZ3Mgd2UgbWFrZSB0aGUgR3JlYXQgV2ViIGNsb3Nlcgo="
+  },
+  QmeaYGKqdJHCstEPsS4iepvT5y6JZyuAKu87kFwz1yQE2x: {
+    rank: "0.000576",
+    grade: { from: 0.000001, to: 0.001, value: 4 },
+    status: "loading",
+    content: "dfcvgbggbg2"
+  },
+  QmeaYGKqdJHCstEPsS4iepvT5y6JZyuAKu87kFwz1yQE3x: {
+    rank: "0.000576",
+    grade: { from: 0.000001, to: 0.001, value: 5 },
+    status: "failed",
+    content: "QmeaYGKqdJHCstEPsS4iepvT5y6JZyuAKu87kFwz1yQE3x"
+  }
+};
+
+const Application = props => {
+  const { openMenu, toggleMenu, newApp } = props;
+
+  return (
+    <Pane
+      height="100vh"
+      width="100vw"
+      backgroundColor="#000"
+      display="flex"
+      alignItems="center"
+    >
+      <Pane padding="2em" width="100%">
+        {Object.keys(links).map(key => (
+          <SearchItem
+            key={key}
+            hash={key}
+            rank={links[key].rank}
+            grade={links[key].grade}
+            status={links[key].status}
+          >
+            {links[key].status !== "success" ? (
+              key
+            ) : (
+              <iframe style={{ width: '100%', border: 'none' }} src={links[key].content} />
+            )}
+          </SearchItem>
+        ))}
+      </Pane>
+    </Pane>
+  );
+};
+
+storiesOf("Molecules|searchSnippet", module).add("searchSnippet", () => (
+  <Application />
+));
