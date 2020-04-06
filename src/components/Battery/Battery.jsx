4b2efbe81f23d4b2cf6c18492e4a5773fa230164
dimakorzhovnik <korzhovnik.dima@gmail.com>
2020-04-06 21:07:45 +0300

    fix Battery

--- src/components/Battery/Battery.jsx
+++ src/components/Battery/Battery.jsx
@@ -1,96 +1,74 @@
-import * as React from 'react';
+import * as React from "react";
 import { Pane, Text, Link, Tooltip } from "evergreen-ui";
-import styles from './Battery.less';
+import styles from "./Battery.less";
 // import cx from 'classnames';
 
 export const Battery = ({
-    children, fontSize, bwPercent, bwRemained, bwMaxValue, linkPrice, contentTooltip, ...props
+  children,
+  fontSize,
+  bwPercent,
+  bwRemained,
+  bwMaxValue,
+  linkPrice,
+  contentTooltip,
+  styleText,
+  colorText,
+  ...props
 }) => (
-    <Tooltip
-      appearance="card"
-      content={ (
-//           <Pane
-//             minWidth={ 200 }
-//             paddingX={ 18 }
-//             paddingY={ 14 }
-//             borderRadius={ 4 }
-//             backgroundColor='#fff'
-//           >
-//               <Pane marginBottom={ 12 }>
-//                   <Text size={ 300 }>You have {bwRemained} BP out of {bwMaxValue} BP.</Text>
-//               </Pane>
-//               <Pane marginBottom={ 12 }>
-//                   <Text size={ 300 }>
-//                         Full regeneration of bandwidth points or BP happens in 24 hours.
-//                   </Text>
-//               </Pane>
-//               <Pane display='flex' marginBottom={ 12 }>
-//                   <Text size={ 300 }>Current rate for 1 cyberlink is {linkPrice} BP.</Text>
-//               </Pane>
-// {/*              <Pane>
-//                   <Text size={ 300 }>
-//                         More on{' '}
-//                       <Link textDecoration='none' color='green' cursor='pointer'>
-//                             Bandwidth
-//                       </Link>
-//                   </Text>
-//               </Pane>*/}
-//           </Pane>
-        contentTooltip
-      ) }
+  <Tooltip appearance="card" content={contentTooltip}>
+    <Pane
+      height={25}
+      width="100%"
+      className={
+        bwPercent <= 25
+          ? styles.skillBarHoverRed
+          : bwPercent < 50
+          ? styles.skillBarHoverOrange
+          : bwPercent < 76
+          ? styles.skillBarHoverYellow
+          : styles.skillBarHoverGreen
+      }
+      backgroundColor="#fff"
+      borderRadius={4}
+      position="relative"
+      transition="1s"
+      {...props}
     >
+      <Pane
+        height="100%"
+        borderRadius="inherit"
+        style={{ transition: "width .2s ease-in" }}
+        width={`${bwPercent}%`}
+        backgroundColor={
+          bwPercent <= 25
+            ? "#d32f2f"
+            : bwPercent < 50
+            ? "#f57c00"
+            : bwPercent < 76
+            ? "#fbc02d"
+            : "#4caf50"
+        }
+      >
         <Pane
-          { ...props }
-          height={ 25 }
-          width='100%'
-          className={
-              bwPercent <= 25
-                    ? styles.skillBarHoverRed
-                    : bwPercent < 50
-                    ? styles.skillBarHoverOrange
-                    : bwPercent < 76
-                    ? styles.skillBarHoverYellow
-                    : styles.skillBarHoverGreen
-            }
-          backgroundColor='#fff'
-          borderRadius={ 4 }
-          position='relative'
-          transition='1s'
+          display="flex"
+          alignItems="center"
+          width="100%"
+          height="100%"
+          position="absolute"
+          paddingX={20}
+          justifyContent={bwPercent < 26 ? "flex-end" : "flex-start"}
         >
-            <Pane
-              height='100%'
-              borderRadius='inherit'
-              style={ { transition: 'width .2s ease-in' } }
-              width={ `${bwPercent}%` }
-              backgroundColor={
-                  bwPercent <= 25
-                        ? '#d32f2f'
-                        : bwPercent < 50
-                        ? '#f57c00'
-                        : bwPercent < 76
-                        ? '#fbc02d'
-                        : '#4caf50'
-                }
-            >
-                <Pane
-                  display='flex'
-                  alignItems='center'
-                  width='100%'
-                  height='100%'
-                  position='absolute'
-                  paddingX={ 20 }
-                  justifyContent={ bwPercent < 26 ? 'flex-end' : 'flex-start' }
-                >
-                    <Text
-                      fontSize={ fontSize || 18 }
-                      fontWeight='bold'
-                      color={ bwPercent < 26 ? '#d32f2f' : '#ffffff' }
-                    >
-                        {`${bwPercent} %`}
-                    </Text>
-                </Pane>
-            </Pane>
-            {children}
+          <Text
+            style={styleText}
+            fontSize={fontSize || 18}
+            fontWeight="bold"
+            color={bwPercent < 26 ? "#d32f2f" : colorText || "#ffffff"}
+          >
+            {`${bwPercent} %`}
+          </Text>
         </Pane>
-    </Tooltip>
+      </Pane>
+      {children}
+    </Pane>
+  </Tooltip>
 );
--- src/components/Battery/Battery.stories.jsx
+++ src/components/Battery/Battery.stories.jsx
@@ -1,43 +1,49 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import centered from '@storybook/addon-centered';
-import { Pane, Text, Link } from 'evergreen-ui';
-import { Battery } from '../..';
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import centered from "@storybook/addon-centered";
+import { Pane, Text, Link } from "evergreen-ui";
+import { Battery } from "../..";
 
 const ContentTooltip = ({ bwRemained, bwMaxValue, linkPrice }) => (
-    <Pane minWidth={ 200 } paddingX={ 18 } paddingY={ 14 } borderRadius={ 4 } backgroundColor='#fff'>
-        <Pane marginBottom={ 12 }>
-            <Text size={ 300 }>
-                You have
-                {' '}
-                {bwRemained}
-                {' '}
-BP out of
-                {' '}
-                {bwMaxValue}
-                {' '}
-BP.
-            </Text>
-        </Pane>
-        <Pane marginBottom={ 12 }>
-            <Text size={ 300 }>Full regeneration of bandwidth points or BP happens in 24 hours.</Text>
-        </Pane>
-        <Pane display='flex' marginBottom={ 12 }>
-            <Text size={ 300 }>
-Current rate for 1 cyberlink is
-                {linkPrice}
-                {' '}
-BP.
-            </Text>
-        </Pane>
+  <Pane
+    minWidth={200}
+    paddingX={18}
+    paddingY={14}
+    borderRadius={4}
+    backgroundColor="#fff"
+  >
+    <Pane marginBottom={12}>
+      <Text size={300}>
+        You have {bwRemained} BP out of {bwMaxValue} BP.
+      </Text>
     </Pane>
+    <Pane marginBottom={12}>
+      <Text size={300}>
+        Full regeneration of bandwidth points or BP happens in 24 hours.
+      </Text>
+    </Pane>
+    <Pane display="flex" marginBottom={12}>
+      <Text size={300}>
+        Current rate for 1 cyberlink is
+        {linkPrice} BP.
+      </Text>
+    </Pane>
+  </Pane>
 );
 
-storiesOf('Atoms|Battery', module).add('Battery', () => (
-    <div style={ { top: '50%', position: 'relative', padding: '0 1em' } }>
-        <Battery
-          bwPercent={ 50 }
-          contentTooltip={ <ContentTooltip bwRemained={ 12 } bwMaxValue={ 100 } linkPrice={ 1 } /> }
-        />
-    </div>
+storiesOf("Atoms|Battery", module).add("Battery", () => (
+  <div style={{ top: "50%", position: "relative", padding: "0 1em" }}>
+    <Pane width="60px">
+      <Battery
+        height={10}
+        styleText={{ whiteSpace: "nowrap" }}
+        fontSize={9}
+        colorText="#000"
+        bwPercent={50}
+        contentTooltip={
+          <ContentTooltip bwRemained={12} bwMaxValue={100} linkPrice={1} />
+        }
+      />
+    </Pane>
+  </div>
 ));
