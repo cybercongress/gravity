61e1abfb6e9ec704af8e79f9feb3ad016f5819e9
Master <starodubcev@gmail.com>
2021-11-30 07:02:39 +0000

    refactor(vitalick): Remove unused pictures

--- src/components/Vitalick/vitalick.jsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import styles from './vitalick.less';
-// import cx from 'classnames';
-
-export const Vitalick = () => (
-    <div className={ styles.vitalick } alt='vitalick' />
-    // <img className={ styles.vitalick } src={ vitalick } alt='vitalick' />
-);
